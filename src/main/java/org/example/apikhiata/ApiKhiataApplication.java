538d459dd6850fc62f6a3819dcb5398d8e687b0e
HeitorCarvalhoCampos <heitor.campos@germinare.org.br>
2024-08-29 11:59:48 -0300

    Vinculando projeto

---
+++ src/main/java/org/example/apikhiata/ApiKhiataApplication.java
@@ -0,0 +1,13 @@
+package org.example.apikhiata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKhiataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiKhiataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/apikhiata/ApiKhiataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apikhiata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKhiataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
