37150f78485f03d9a1c6e5d91485e674c9aedd5c
NicolasNakashima <nicolas.nakashima@germinare.org.br>
2024-10-25 11:41:15 -0300

    cors

--- src/main/java/org/example/apikhiata/config/WebConfig.java
+++ src/main/java/org/example/apikhiata/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Define o caminho que terá CORS habilitado
                 .allowedOrigins("http://localhost:3000") // Permite requisições de localhost:3000
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedMethods("*") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos os cabeçalhos
                 .allowCredentials(true) // Permite envio de cookies/credenciais
                 .maxAge(3600); // Define por quanto tempo a resposta CORS pode ser armazenada em cache pelo navegador
