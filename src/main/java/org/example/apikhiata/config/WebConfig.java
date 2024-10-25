d1345dbbb0ae9ebbfd34d4ef087d83e4e52f7cf0
NicolasNakashima <nicolas.nakashima@germinare.org.br>
2024-10-25 09:42:30 -0300

    adicionando WebConfig e configurando CORS

---
+++ src/main/java/org/example/apikhiata/config/WebConfig.java
@@ -0,0 +1,20 @@
+package org.example.apikhiata.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Define o caminho que terá CORS habilitado
+                .allowedOrigins("http://localhost:3000") // Permite requisições de localhost:3000
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos os cabeçalhos
+                .allowCredentials(true) // Permite envio de cookies/credenciais
+                .maxAge(3600); // Define por quanto tempo a resposta CORS pode ser armazenada em cache pelo navegador
+    }
+}
